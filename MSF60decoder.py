b7219120c248205684eef8c8feea880485d2bdbe
Tony Robinson <tonyr@tonyRobinson.com>
2019-06-05 07:12:25 +0100

    Improved time resolution

--- MSF60decoder.py
+++ MSF60decoder.py
@@ -17,9 +17,9 @@ import matplotlib.pyplot as plt
 
 # fundamental parameters
 symbolTime = 0.1  # this is the duration of a symbol (one bit) in seconds
-overSample = 8    # we oversample so as to locate the start of every second
+overSample = 8    # we oversample to easily locate the start of every symbol
 
-# rough check of format (ignores fixed A and B values)
+# rough check of format (ignores fixed A and B values for now)
 def formatCheck(frames):
   check = (frames[0] == [True, True, True, True, True, False, False, False, False, False])
   if check:
@@ -27,16 +27,13 @@ def formatCheck(frames):
       check = check and frame[0]
       for posn in range(3, 10):
         check = check and not frame[posn]
-
   return check
 
 # check a block of frames against its CRC value
 def crcCheck(frames, crc):
   parity = True
-
   for frame in frames:
    parity ^= frame[1]
-
   return parity == crc[2]
    
 # decode a set of BCD frames and report the number of bit errors 
@@ -55,8 +52,8 @@ def BCDdecode(frames):
 if len(sys.argv) != 2:
   exit('Symtax: %s file.wav' % (sys.argv[0]))
 
-sampleRate, wavdata = scipy.io.wavfile.read(sys.argv[1])
-data = wavdata.astype(float)
+sampleRate, wavData = scipy.io.wavfile.read(sys.argv[1])
+data = wavData.astype(float)
 ## plt.plot(data[0:512])
 ## plt.semilogy(abs(np.fft.fft(data[0:sampleRate])[0:int(sampleRate/2)]))
 
@@ -81,28 +78,22 @@ for init in range(0, len(data) - windowLen, int(windowLen/overSample)):
   demod.append(np.sqrt(ssum * ssum + csum * csum))
 ## plt.plot(demod)
 
-# the carrier is on at least 50% of the time, use top half
-hiValue = np.mean(sorted(demod, reverse = True)[0:int(0.5 * len(demod))])
-# and there is always one in 10 that is low, so use bottom 10%
-loValue = np.mean(sorted(demod)[0:int(0.1 * len(demod))])
-
-# this really should be calculated after the decode
-## print("approxSNR: %.0f dB" % (20 * np.log10(hiValue / loValue)))
-
-# now we have a sensible threshold to convert demod into bits
-threshold = 0.5 * (loValue + hiValue)
-
-# find the start of the second which is always high to low
+# find the start of a symbol which is always low, so find the lowest point
 period = int(overSample / symbolTime)
 acc = [0] * period  
-for i in range(1, len(demod)):
-  if demod[i - 1] > threshold and demod[i] < threshold:
-    acc[i % period] += 1
-offset = acc.index(max(acc))
+for i in range(0, len(demod) - period):
+  acc[i % period] += demod[i]
+offset = acc.index(min(acc))
+
+# average these low bits and also the previous bit which is always high
+nperiod = (len(demod) - period) / period
+loValue = acc[offset] / nperiod
+hiValue = acc[(offset - overSample + period) % period] / nperiod
+threshold = 0.5 * (loValue + hiValue)
+## print("approxSNR: %.0f dB" % (20 * np.log10(hiValue / loValue)))
 
 # decode all the bits
 bout = []
-minit = -1
 for init in range(offset, len(demod) - period, period):
   frame = []
   for i in range(0, period, overSample):
@@ -130,6 +121,6 @@ for i in range(0, len(bout) - 60):
     # we could compute the true SNR here as now we know all the bits
 
     radioTime = datetime.datetime(year, month, dayOM, hour=hour, minute=minute).timestamp()
-    systemTime = os.path.getmtime(sys.argv[1]) + i + 60 - 0.59 - len(bout)
+    systemTime = os.path.getmtime(sys.argv[1]) - len(wavData)/sampleRate + i + 60 + offset * symbolTime/overSample
     
-    print("packet: %4d-%02d-%02d %02d:%02d    sysDiff %+0.2f" % (year, month, dayOM, hour, minute, systemTime - radioTime))
+    print("packet: %4d-%02d-%02d %02d:%02d    sysDiff %+0.3f" % (year, month, dayOM, hour, minute, radioTime - systemTime))
--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ if [ ! -f test.wav ] ; then
   bunzip2 test.wav.bz2
 fi
    
-# this should yield: packet: 2019-05-31 13:49    sysDiff +0.5
+# this should yield: packet: 2019-05-31 13:49    sysDiff +0.025
 python3 MSF60decoder.py test.wav
 
 exit 0
